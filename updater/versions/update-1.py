1c878ae93773a3acb63ec192523bd4ad0ddbae2f
Arturo Filastò <arturo@filasto.net>
2016-09-05 17:02:10 +0200

    Update the updater

--- updater/versions/update-1.py
+++ updater/versions/update-1.py
@@ -1,3 +1,16 @@
+"""
+This is the auto update script for going from version 0 to version 1.
+
+All future lepidopter will start with version 1 already configured, so this should
+not be run by them.
+
+In order to indicate that they are configured with version 1 a file called
+/etc/lepidopter-update/version with the string 1.
+
+When this is run it will configure the pi to be already initialized and this
+should not happen in future versions of lepidopter.
+"""
+
 import os
 import shutil
 
@@ -7,7 +20,7 @@ DEB_RELEASE="jessie"
 TOR_DEB_REPO="http://deb.torproject.org/torproject.org"
 TOR_DEB_REPO_SRC_LIST="/etc/apt/sources.list.d/tor.list"
 TOR_REPO_GPG="A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89"
-OONIPROBE_PIP_URL = "git+https://github.com/TheTorProject/ooni-probe@v2.0.0-alpha#egg=ooniprobe"
+OONIPROBE_PIP_URL = "https://people.torproject.org/~art/ooni/ooniprobe-2.0.0a3.tar.gz"
 
 OONIPROBE_SYSTEMD_SCRIPT = """\
 [Unit]
@@ -28,6 +41,15 @@ WantedBy=multi-user.target
 """
 OONIPROBE_SYSTEMD_PATH = "/etc/systemd/system/ooniprobe.service"
 
+OONIPROBE_CONFIG = """
+basic:
+   logfile: /var/log/ooni/ooniprobe.log
+advanced:
+   webui_port: 8842
+   webui_address: "0.0.0.0"
+"""
+OONIPROBE_CONFIG_PATH = "/etc/ooniprobe.conf"
+
 def rm_rf(path):
     if os.path.isdir(path):
         shutil.rmtree(path, ignore_errors=True)
@@ -41,6 +63,10 @@ def write_systemd_script():
     with open(OONIPROBE_SYSTEMD_PATH, "w") as out_file:
         out_file.write(OONIPROBE_SYSTEMD_SCRIPT)
 
+def write_ooniprobe_config():
+    with open(OONIPROBE_CONFIG_PATH, "w") as out_file:
+        out_file.write(OONIPROBE_CONFIG)
+
 def run():
     # Delete all the daily crons
     rm_rf("/etc/cron.daily/remove_upl_reports")
@@ -52,7 +78,7 @@ def run():
     # XXX this is still present in the lepidopter v2 branch.
     rm_rf("/etc/cron.daily/update_ooniprobe")
 
-    rm_rf("/etc/ooniprobe/oonireport.conf")
+    rm_rf("/etc/ooniprobe")
 
     rm_rf("/opt/ooni/remove-inc-reports.sh")
     rm_rf("/opt/ooni/remove-upl-reports.sh")
@@ -75,10 +101,18 @@ def run():
         out_file.write("deb {0} {1} main".format(TOR_DEB_REPO, DEB_RELEASE))
     check_call(["apt-get", "update"])
 
+    write_systemd_script()
+    write_ooniprobe_config()
+
     # Install obfs4proxy that includes a lite version of meek
     check_call(["apt-get", "-y", "install", "-t", "stretch", "obfs4proxy"])
 
-    check_call(["pip", "install", OONIPROBE_PIP_URL])
+    check_call(["pip", "install", "--upgrade", OONIPROBE_PIP_URL])
+
+    # Set it as already initialized so we skip the informed consent on
+    # pre-existing ooniprobes.
+    with open("/var/lib/ooni/initialized", "w"):
+        pass
 
     check_call(["systemctl", "enable", "ooniprobe"])
     check_call(["systemctl", "start", "ooniprobe"])
